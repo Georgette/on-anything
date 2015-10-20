24f460d2ac3cfc1062407139b4c1d495eec3a3f6
Georgette Pincin <georgette@pincin.com>
2015-10-20 17:59:08 -0400

    finishing off functionality with tests

---
+++ test/on-off.js
@@ -0,0 +1,76 @@
+var test         = require('tape'),
+    EventEmitter = require('events').EventEmitter,
+    on           = require('../on'),
+    off          = require('../off'),
+    $            = require('jquery')
+
+test('on should bind and unbind a handler on EventEmitters', function (t) {
+    t.plan(2)
+
+    var emitter = new EventEmitter()
+    var count = 0
+    var cb = function (request) {
+        t.equal(request.url, 'http://localhost/hello', 'should handle request event')
+        count++
+    }
+
+    on(emitter, 'request', cb)
+
+    emitter.emit('request', { url: 'http://localhost/hello' })
+
+    off(emitter, 'request', cb)
+
+    emitter.emit('request', { url: 'http://localhost/hello' })
+
+    t.equal(count, 1, 'should no longer be accepting events')
+
+})
+
+test('on should bind and unbind jquery emitter', function (t) {
+    t.plan(2)
+
+    var element = $('<div></div>')
+    var count = 0
+    var cb = function (event) {
+        t.equal(event.type, 'request', 'should handle request event')
+        count++
+    }
+
+    on(element, 'request', cb)
+
+    element.trigger('request')
+
+    off(element, 'request', cb)
+
+    element.trigger('request')
+
+    t.equal(count, 1, 'should no longer be accepting events')
+
+})
+
+test('on should bind and unbind addEventListener', function (t) {
+    t.plan(2)
+
+    var element = document.createElement('DIV')
+    var count = 0
+
+    var clickEvent = document.createEvent('MouseEvents')
+    clickEvent.initMouseEvent('click', true, true, window,
+        0, 0, 0, 0, 0, false, false, false, false, 0, null)
+
+    var cb = function (event) {
+        t.equal(event.type, 'click', 'should handle click event')
+        count++
+    }
+
+    on(element, 'click', cb)
+
+    element.dispatchEvent(clickEvent)
+
+    off(element, 'click', cb)
+
+    element.dispatchEvent(clickEvent)
+
+    t.equal(count, 1, 'should no longer be accepting events')
+
+})
